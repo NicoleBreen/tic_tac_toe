a1986f818ee1d5ae8563c58127f57bda46e8b908
Nicole <n.breen@hotmail.co.uk>
2024-10-10 16:07:56 +0100

    User input

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,6 +7,11 @@ row1 = [' ',' ', ' ']
 row2 = [' ',' ', ' '] 
 row3 = [' ',' ', ' '] 
 
+# Example of putting an X at a index position
 row2[1] = 'X'
 
+# Display the rows
 display(row1,row2,row3)
+
+# Ask user for index position, change from str to int
+position_index = int(input("Choose an index position: "))
