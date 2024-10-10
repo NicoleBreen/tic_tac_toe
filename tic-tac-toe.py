73b8fdfad5c99cbb5d69060c9d81fdff9d2baff1
Nicole <n.breen@hotmail.co.uk>
2024-10-10 15:36:49 +0100

    display

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,12 @@
-print('this is a change')
+def display(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+row1 = [' ',' ', ' '] 
+row2 = [' ',' ', ' '] 
+row3 = [' ',' ', ' '] 
+
+row2[1] = 'X'
+
+display(row1,row2,row3)
