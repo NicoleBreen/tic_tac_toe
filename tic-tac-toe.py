d4a9f8292b2518ba3ba321db888b04489ccf3a24
Nicole <n.breen@hotmail.co.uk>
2024-10-11 13:54:25 +0100

    user_validation

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,5 +13,28 @@ row2[1] = 'X'
 # Display the rows
 display(row1,row2,row3)
 
+def user_choice():
+    choice = 'WRONG'
+    acceptable_range = range(0,4)
+    within_range = False
+
+    # Can use .isdigit() this translates a string number
+    while choice.isdigit() == False or within_range == False:
+        choice = input ("Please enter a number (0-3): ")
+        if choice.isdigit() == False:
+            print("Sorry, that is not a digit!")
+        if choice.isdigit() == True:
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print("Sorry, you are out of acceptable range (0 to 3)")
+                within_range = False
+
+    return int(choice)
+
+user_choice()
+
 # Ask user for index position, change from str to int
-position_index = int(input("Choose an index position: "))
+#position_index = int(input("Choose an index position: "))
+
+
